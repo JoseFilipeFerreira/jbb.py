8fc8997d8e5ec747a9a8cc7799eeff960b9ed132
JoseFilipeFerreira <36403066+JoseFilipeFerreira@users.noreply.github.com>
2018-06-11 15:17:12 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -15,6 +15,16 @@ class Manage():
         if ctx.message.author == appInfo.owner:
             await self.bot.change_presence(game=discord.Game(name='rebooting'))
             subprocess.call("./update.sh")
+        else:
+            wait self.bot.say("Invalid User")
+    
+    @commands.command(pass_context=True)
+    async def stdplay(self, ctx):
+        appInfo = await self.bot.application_info()
+        if ctx.message.author == appInfo.owner:
+            await self.bot.change_presence(game=discord.Game(name='*help'))
+        else:
+            wait self.bot.say("Invalid User")
 
 def setup(bot):
     bot.add_cog(Manage(bot))
