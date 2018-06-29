726872786aa65f618b6c56b3bd6e3a2243c02e0b
José FerreIra <jose.filipe.matos.ferreira@gmail.com>
2018-06-29 18:53:01 +0100

    memegenertaor resizes text

--- memegenerator.py
+++ memegenerator.py
@@ -14,22 +14,30 @@ class Memegenerator():
         self.bot = bot
     
     @commands.command(pass_context=True)
-    async def meme(self, ctx, image, top, bottom):
-        if (not top): top = " "
-        if (not bottom): bottom = " "
+    async def meme(self, ctx, image, *text):
+        if (len(text) == 0):
+            top = " "
+            bottom = " "
+        elif(len(text) == 1):
+            top = text[0]
+            bottom = " "
+        else:
+            top = text[0]
+            bottom = text[1] #"parse" information to draw
+
         img = Image.open("Images/{}.png".format(image))
         draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("impact.ttf", 42)
-        w1, h1 = draw.textsize(top, font) # measure the size the text will take
-        w2, h2 = draw.textsize(bottom, font)
-        drawTextWithOutline(draw, font, top, img.width/2 - w1/2, 10)
-        drawTextWithOutline(draw, font, bottom, img.width/2 - w2/2, img.height-52)
+        
+        fontTop, w1, h1 = getFittingFont(img, "impact.ttf", top)
+        fontBottom, w2, h2 = getFittingFont(img, "impact.ttf", bottom)
 
-        name = getRandomName(20)
-        img.save("Memegenerator/{}.png".format(name))
+        drawTextWithOutline(draw, fontTop, top, img.width/2 - w1/2, 10)
+        drawTextWithOutline(draw, fontBottom, bottom, img.width/2 - w2/2, img.height-52)
+
+        img.save("Memegenerator/tmp.png")
     
         await self.bot.delete_message(ctx.message)
-        await self.bot.send_file(ctx.message.channel, "Memegenerator/{}.png".format(name))
+        await self.bot.send_file(ctx.message.channel, "Memegenerator/tmp.png")
         await self.bot.say('by {}'.format(ctx.message.author.mention))
 
 def setup(bot):
@@ -46,3 +54,15 @@ def drawTextWithOutline(draw, font, text, x, y):
 
 def getRandomName(size):
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
+
+#get font that fits in image
+def getFittingFont(img, fontName, text):
+    textSize = 42
+    font = ImageFont.truetype(fontName, textSize)
+    w, h = font.getsize(text)
+    if (w > img.width and textSize > 1):
+        while(w > img.width and textSize > 1):
+            textSize = textSize - 1
+            font = ImageFont.truetype(fontName, textSize)
+            w, h = font.getsize(text)
+    return font, w, h
