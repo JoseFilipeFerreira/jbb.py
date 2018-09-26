43d1a568ae9a8bc00abdbf17bcd2a1efb6cba7ec
jose.filipe.matos.ferreira <jose.filipe.matos.ferreira@gmail.com>
2018-09-26 21:25:19 +0100

    Add serverinfo

--- Extensions/manage.py
+++ Extensions/manage.py
@@ -51,8 +51,8 @@ class Manage():
                 embed_colour = member.colour.value
             embed = discord.Embed(title=str(user), url=user.avatar_url, description=user.display_name, color=embed_colour)
             embed.set_thumbnail(url=user.avatar_url)
-            embed.add_field(name='Is bot:', value=user.bot, inline=True)
-            embed.add_field(name='Voice channel:', value=user.voice_channel, inline=True)
+            embed.add_field(name='Is bot', value=user.bot, inline=True)
+            embed.add_field(name='Voice channel', value=user.voice_channel, inline=True)
             role_list = "None"
             if len(member.roles) > 1:
                 role_array = []
@@ -61,13 +61,43 @@ class Manage():
                 role_array.pop(0)
                 role_array.reverse()
                 role_list = ', '.join(role_array)
-            embed.add_field(name='Roles:', value=role_list, inline=False)
-            embed.add_field(name='Playing:', value=member.game, inline=False)
-            embed.add_field(name='Joined discord at:', value=user.created_at, inline=True)
-            embed.add_field(name='Joined server at:', value=member.joined_at, inline=True)
+            embed.add_field(name='Roles', value=role_list, inline=False)
+            embed.add_field(name='Playing', value=member.game, inline=False)
+            embed.add_field(name='Joined discord at', value=user.created_at, inline=True)
+            embed.add_field(name='Joined server at', value=member.joined_at, inline=True)
             await self.bot.say(embed=embed)
 
-
+    @commands.command(pass_context=True)
+    async def serverinfo(self, ctx):
+    #get info on a specific user
+        server = ctx.message.server
+        total = len(ctx.message.server.members)
+        bot  = 0
+        online = 0
+        for member in server.members:
+            if member.bot:
+                bot = bot + 1
+            if member.status != discord.Status.offline:
+                online = online + 1
+        embed = discord.Embed(title="serverInfo", description=server.name, color=0xffff00)
+        embed.set_thumbnail(url=server.icon_url)
+        embed.add_field(name='Owner', value=server.owner, inline=False)
+        embed.add_field(name='Region', value=server.region, inline=False)
+        text_channel = 0
+        voice_channel = 0
+        for channel in server.channels:
+            if channel.type == discord.ChannelType.text:
+                text_channel = text_channel + 1
+            elif channel.type == discord.ChannelType.voice:
+                voice_channel = voice_channel + 1
+        embed.add_field(name='Text Channels', value=text_channel, inline=False)
+        embed.add_field(name='Voice Channels', value=voice_channel, inline=False)
+        embed.add_field(name='Members', value=total, inline=False)
+        embed.add_field(name='Humans', value=total-bot, inline=False)
+        embed.add_field(name='Bots', value=bot, inline=False)
+        embed.add_field(name='Online', value=online, inline=False)
+        embed.add_field(name='Roles', value=len(server.roles), inline=False)
+        await self.bot.say(embed=embed)
 
 
 def setup(bot):
