762b789f02c76f37eacfe2ac19fa443246e4b8d5
Hitler <a85504@alunos.uminho.pt>
2018-11-01 11:37:07 +0000

    use random.choice to choose embed color

--- Extensions/dogs.py
+++ Extensions/dogs.py
@@ -20,8 +20,7 @@ class Dogs():
             else:
                 isVideo = False
                 colours = [0x1abc9c, 0x11806a, 0x2ecc71, 0x1f8b4c, 0x3498db, 0x206694, 0x9b59b6, 0x71368a, 0xe91e63, 0xad1457, 0xf1c40f, 0xc27c0e, 0xa84300, 0xe74c3c, 0x992d22, 0x95a5a6, 0x607d8b, 0x979c9f, 0x546e7a]
-                col = int(random.random() * len(colours))
-                em = discord.Embed(color=colours[col])
+                em = discord.Embed(color=random.choice(colours))
                 em.set_image(url=js['url'])
                 await self.bot.say(embed=em) 
 
