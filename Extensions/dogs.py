30334389420906da7facdd764553a20cc04f6fc3
jose.filipe.matos.ferreira <jose.filipe.matos.ferreira@gmail.com>
2018-11-03 14:43:55 +0000

    cat antiauau

--- Extensions/dogs.py
+++ Extensions/dogs.py
@@ -1,7 +1,7 @@
 import discord
 import asyncio
 import requests
-import random
+from random import choice
 from discord.ext import commands
 
 class Dogs():
@@ -20,9 +20,18 @@ class Dogs():
             else:
                 isVideo = False
                 colours = [0x1abc9c, 0x11806a, 0x2ecc71, 0x1f8b4c, 0x3498db, 0x206694, 0x9b59b6, 0x71368a, 0xe91e63, 0xad1457, 0xf1c40f, 0xc27c0e, 0xa84300, 0xe74c3c, 0x992d22, 0x95a5a6, 0x607d8b, 0x979c9f, 0x546e7a]
-                em = discord.Embed(color=random.choice(colours))
-                em.set_image(url=js['url'])
-                await self.bot.say(embed=em) 
+                embed = discord.Embed(color=choice(colours))
+                embed.set_image(url=js['url'])
+                await self.bot.say(embed=embed)
+
+    @commands.command(pass_context=True, aliases=['antiauau', 'miau'])
+    async def cat(self, ctx):
+        r =requests.get('http://aws.random.cat/meow')
+                
+        colours = [0x1abc9c, 0x11806a, 0x2ecc71, 0x1f8b4c, 0x3498db, 0x206694, 0x9b59b6, 0x71368a, 0xe91e63, 0xad1457, 0xf1c40f, 0xc27c0e, 0xa84300, 0xe74c3c, 0x992d22, 0x95a5a6, 0x607d8b, 0x979c9f, 0x546e7a]
+        embed = discord.Embed(color=choice(colours))
+        embed.set_image(url=r.json()['file'])
+        await self.bot.say(embed=embed) 
 
 def setup(bot):
     bot.add_cog(Dogs(bot))
