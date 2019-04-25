cef5da527e8851b405ce15431069c1e7b6d97e26
MightyMime <jose.filipe.matos.ferreira@gmail.com>
2019-04-25 19:45:46 +0100

    restrict casino to nsfw

--- Extensions/casino.py
+++ Extensions/casino.py
@@ -59,9 +59,9 @@ class Casino():
                       brief="Play roulette",
                       pass_context=True)
     async def roulette(self, ctx, amount, bet):
-        if ctx.message.channel.name not in ['nsfw', 'bot-commands']:
+        if ctx.message.channel.name not in ['nsfw']:
             await self.bot.say(
-                "This command must be done in #nsfw or #bot-commands"
+                "This command must be done in #nsfw"
             )
             return
         rOrder = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26]
@@ -174,9 +174,9 @@ class Casino():
                       brief="roll a dice",
                       pass_context=True)
     async def roll(self, ctx, * bet):
-        if ctx.message.channel.name not in ['nsfw', 'bot-commands']:
+        if ctx.message.channel.name not in ['nsfw']:
             await self.bot.say(
-                "This command must be done in #nsfw or #bot-commands"
+                "This command must be done in #nsfw"
             )
             return
         if len(bet) == 0:
@@ -233,9 +233,9 @@ class Casino():
                       brief="slot machine",
                       pass_context=True)
     async def slot(self, ctx, amount):
-        if ctx.message.channel.name not in ['nsfw', 'bot-commands']:
+        if ctx.message.channel.name not in ['nsfw']:
             await self.bot.say(
-                "This command must be done in #nsfw or #bot-commands"
+                "This command must be done in #nsfw"
             )
             return
         wheels_array = []
