84abf49366f52e4f4bd18a50afae77b4d5aab54e
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2020-06-14 12:48:57 +0100

    add resumos

--- extensions/dotfiles.py
+++ extensions/dotfiles.py
@@ -54,6 +54,71 @@ class Dotfiles(commands.Cog):
             else:
                 await ctx.send(searched_repo.html_url)
 
+    @commands.command(
+        name='resumos',
+        description="manage resumos\n\nresumos\nresumos [FILE/FOLDER]\nresumos [FOLDER] [FILE]",
+        brief="manage resumos")
+    async def resumos(self, ctx,* args):
+        site = 'https://git.mendess.xyz/ResumosMIEI/'
+        repo = 'mendess/ResumosMIEI'
+
+        if len(args) == 0:
+            await ctx.send(site)
+            return
+
+        repo = self.g.get_repo(repo)
+        contents = repo.get_contents("")
+
+        searched_repo = None
+
+        if len(args) == 1:
+            while contents:
+                file_content = contents.pop(0)
+                if args[0].lower() in file_content.path.lower():
+                    searched_repo = file_content
+                    break
+                elif file_content.type == "dir":
+                    contents.extend(repo.get_contents(file_content.path))
+
+        elif len(args) == 2:
+            folder = args[0].lower()
+            file = args[1].lower()
+
+            folder_contents = None
+            while contents:
+                content = contents.pop(0)
+                if content.type == "dir" and folder in content.path.lower():
+                    folder_contents = repo.get_contents(content.path)
+                    break
+
+            if not folder_contents:
+                await ctx.send('Folder does not exist')
+                return
+
+            while folder_contents:
+                file_content = folder_contents.pop(0)
+                if file_content.type == "dir":
+                    folder_contents.extend(repo.get_contents(file_content.path))
+                elif file in file_content.name.lower():
+                    searched_repo = file_content
+                    break
+        else:
+            await ctx.send('Invalid number of arguments')
+
+        if not searched_repo:
+            await ctx.send("No file found")
+            return
+
+        web = searched_repo.html_url
+        web = web.replace(
+            'https://github.com/mendess/ResumosMIEI/blob/master/',
+            site)
+        web = web.replace(
+            'https://github.com/mendess/ResumosMIEI/tree/master/',
+            site)
+        if '.' in web.split('/')[-1]:
+            web = web.replace(web.split('.')[-1], 'html')
+        await ctx.send(web)
+
 def setup(bot):
     bot.add_cog(Dotfiles(bot))
-
