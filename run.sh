51ec2945f9168375ff4ce3e03f15e0d98c0bce4b
JoseFilipeFerreira <jose.filipe.ferreira@gmail.com>
2020-09-18 14:06:05 +0100

    fix run.sh command

--- run.sh
+++ run.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
-cd $(dirname "$0")
-
-until ping -q -c 1 www.github.com &> /dev/null
-do
-    true
-done
-
 git pull
 
 if [ -e '.env' ]
 then
     source '.env/bin/activate'
 else
-    virtualenv --python=python3.7  '.env' || exit 2
+    virtualenv '.env' || exit 2
     source '.env/bin/activate' || exit 3
     pip install -r 'requirements.txt' --upgrade || exit 4
 fi
 
 while true 
-do 
-    python3.7 bot.py
-    git pull
+do
+    python bot.py
     while ! ping -c 1 github.com &> /dev/null; do true; done
+    git pull
 done
